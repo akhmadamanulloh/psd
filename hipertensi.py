af7d40a8dd8b37ac3ecd79ed659276d431e8857e
Akhmad Amanulloh <84214955+akhmadamanulloh@users.noreply.github.com>
2023-06-17 23:01:22 +0700

    Update hipertensi.py

--- hipertensi.py
+++ hipertensi.py
@@ -13,15 +13,22 @@ def load_dataset():
 
 # Preprocess dataset
 def preprocess_data(data):
+    data.fillna(data.mean(), inplace=True)  # Mengisi missing values dengan nilai rata-rata
+    data['sex'] = data['sex'].astype('category')
+    data['cp'] = data['cp'].astype('category')
+    # ...
+    # Konversi fitur kategorikal menjadi numerik menggunakan LabelEncoder
+    le = LabelEncoder()
+    data['sex'] = le.fit_transform(data['sex'])
+    data['cp'] = le.fit_transform(data['cp'])
+    # ...
+    
     features = ['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg',
                 'thalach', 'exang', 'oldpeak', 'slope', 'ca', 'thal']
     target = 'target'
     
     X = data[features]
     y = data[target]
-    
-    le = LabelEncoder()
-    y = le.fit_transform(y)
     
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
@@ -51,45 +58,8 @@ def main():
     
     # User input
     st.title("Deteksi Penyakit Hipertensi")
-    age = st.number_input("Usia", min_value=0)
-    sex = st.selectbox("Jenis Kelamin", ["Male", "Female"])
-    cp = st.selectbox("Jenis Nyeri Dada", [0, 1, 2, 3])
-    trestbps = st.number_input("Tekanan Darah Istirahat", min_value=0)
-    chol = st.number_input("Kolesterol", min_value=0)
-    fbs = st.selectbox("Gula Darah Puasa", [0, 1])
-    restecg = st.selectbox("EKG Istirahat", [0, 1, 2])
-    thalach = st.number_input("Denyut Jantung Maksimal", min_value=0)
-    exang = st.selectbox("Angina yang diinduksi olahraga", [0, 1])
-    oldpeak = st.number_input("Depresi ST yang Dicetak oleh Latihan Relatif terhadap Istirahat", min_value=0)
-    slope = st.selectbox("Kemiringan Segment ST", [0, 1, 2])
-    ca = st.selectbox("Jumlah Pembuluh Utama yang Dicat", [0, 1, 2, 3])
-    thal = st.selectbox("Hasil Tes Thalium", [0, 1, 2, 3])
+    # ...
+    # Tambahkan input pengguna dan kode prediksi seperti sebelumnya
     
-    input_data = pd.DataFrame({
-        'age': [age],
-        'sex': [sex],
-        'cp': [cp],
-        'trestbps': [trestbps],
-        'chol': [chol],
-        'fbs': [fbs],
-        'restecg': [restecg],
-        'thalach': [thalach],
-        'exang': [exang],
-        'oldpeak': [oldpeak],
-        'slope': [slope],
-        'ca': [ca],
-        'thal': [thal]
-    })
-    
-    # Perform prediction
-    prediction = predict(model, input_data)
-    
-    # Display result
-    st.subheader("Hasil Prediksi")
-    if prediction[0] == 0:
-        st.write("Tidak Terdiagnosis Hipertensi")
-    else:
-        st.write("Terdiagnosis Hipertensi")
-
-if __name__ == '__main__':
-    main()
+    if __name__ == '__main__':
+        main()
